d759ae7e00a93269e5f53dbaf56c71f8d8ce1607
RomanZemin <RomanZemin12@yandex.ru>
2023-03-21 17:42:09 +0700

    new vers

--- RomkaGPT/RomkaGPT.py
+++ RomkaGPT/RomkaGPT.py
@@ -4,10 +4,16 @@ from multiprocessing import context
 import os
 import openai
 import telebot
+import threading
+import requests
+import base64
+import json
+import shutil
+import time
 from telebot import types
 from datetime import datetime
 
-openai.api_key = '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'
+openai.api_key = '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'
 bot = telebot.TeleBot("6176959976:AAET3wYqmNPV3GSSuMHPxlECq3unFzKYBAQ")
 user_contexts = {}
 
@@ -15,6 +21,7 @@ user_contexts = {}
 
 def handle_message(message):
     global user_contexts
+
     user_id = message.from_user.id
     if user_id not in user_contexts:
         user_contexts[user_id] = ""
@@ -46,7 +53,55 @@ def handle_message(message):
 
     send_msg(message.from_user.id, info)
 
+def check_for_updates():
+  while True:
+    current_version = get_current_version()
+    version = get_new_version()
+    if current_version != version:
+      download_update()
+      restart_application()
+    time.sleep(1000)
 
+def get_current_version():
+  with open('cur_version.txt', 'r') as f:
+    return f.read()
+    
+def get_new_version():
+    current_version = get_current_version()
+
+    response = requests.get('https://api.github.com/repos/RomanZemin/RomkaGPT/contents/version.txt')
+    if response.status_code == requests.codes.ok:
+        jsonResponse = response.json()  # the response is a JSON
+        #the JSON is encoded in base 64, hence decode it
+        content = base64.b64decode(jsonResponse['content'])
+        #convert the byte stream to string
+        jsonString = content.decode('utf-8')
+        finalJson = json.loads(jsonString)  
+        version = str(content).replace("b\'","").replace("\\n\'","")
+    else:
+        version = current_version
+        print('Content was not found.')
+    return version
+
+def download_update():
+  version = get_new_version()
+  url = 'https://github.com/RomanZemin/RomkaGPT/raw/master/RomkaGPT/dist/RomkaGPT.exe'
+  r = requests.get(url, stream=True)
+  if r.status_code == 200:
+    with open(f'dist/RomkaGPT_{version}.exe', 'wb') as f: #открываем exe файл дл€ записи, использу€ бинарный режим "wb"
+        r.raw.decode_content = True
+        shutil.copyfileobj(r.raw, f) #сохран€ем содержимое ответа в файл
+  with open('cur_version.txt', 'w') as f:
+    f.write(version)
+
+def restart_application():
+  version = get_new_version()
+  current_version = get_current_version()
+  time.sleep(1000)
+  os.startfile(f'dist/RomkaGPT_{version}.exe')
+
+  os.system(f'taskkill /f /im RomkaGPT_{current_version}.exe')
+  print('open')
 
 def send_msg(id, info):
     if len(info) > 4096:
@@ -54,7 +109,6 @@ def send_msg(id, info):
            bot.send_message(chat_id=id, text=info[x:x+4096])
     else:
         bot.send_message(chat_id=id, text=info)
-
 
 def get_response(prompt_text)-> str:
     response = openai.Completion.create(
@@ -69,5 +123,6 @@ def get_response(prompt_text)-> str:
     )   
     return response['choices'][0]['text'].lstrip('\n')
 
-
-bot.infinity_polling()
+if __name__ == '__main__':
+  threading.Thread(target=check_for_updates).start()
+  bot.infinity_polling()
